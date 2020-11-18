75c46aba5f8b236bbc0b7c17ffa0da4f951d21f7
Hasnain Ahmad Tanim <hatanim987@users.noreply.github.com>
2020-11-19 00:48:42 +0600

    2nd Test Commit

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,17 @@ package com.example.prayergraph;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
+
+        Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
     }
 }
