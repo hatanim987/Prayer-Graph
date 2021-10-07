57e393007920d986c8d25ebbbb3d7eff9d8f1472
Hasnain Ahmad Tanim <hatanim987@gmail.com>
2021-10-07 23:58:25 +0600

    Testing

--- MainActivity.java
+++ MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
 
         Toast.makeText(getApplicationContext(),"Hello Muslims!",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
